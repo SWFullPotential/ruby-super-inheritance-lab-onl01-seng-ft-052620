1f7c549ebd46798879f9d2114b30a89b926b5239
Samantha Wabi <swfullpotential@gmail.com>
2020-06-11 23:18:25 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,1 +1,14 @@
-require_relative "./"
+require_relative "./student.rb"
+
+class ChattyStudent < Student 
+  def raise_hand
+    puts "Pick me!"
+  end
+  
+  def hello
+    super
+    puts 
+  end
+  end
+  
+end
